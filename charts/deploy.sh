828aa548de1903bd33969dc2af285cb658cdd381
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-10-15 14:58:25 +0300

    order user service communication fixed

--- charts/deploy.sh
+++ charts/deploy.sh
@@ -1,15 +1,25 @@
 helm install auth-postgres bitnami/postgresql -f auth/postgres/postgres-values.yaml
-helm install user-postgres bitnami/postgresql -f postgres-values.yaml
-helm install user-redis bitnami/redis -f redis-values.yaml
+helm install user-postgres bitnami/postgresql -f user/postgres/postgres-values.yaml
+helm install order-postgres bitnami/postgresql -f order/postgres/postgres-values.yaml
+helm install user-redis bitnami/redis -f user/redis/redis-values.yaml
+
+cd kafka || exit
+helm install kafka .
+cd ..
 
 sleep 10
 
 cd auth/microservice || exit
 helm dependency update
 helm install auth-service-release .
-
 cd ../..
+
 cd user/microservice || exit
 helm dependency update
 helm install user-service-release .
+cd ../..
+
+cd order/microservice || exit
+helm dependency update
+helm install order-service-release .
 cd ../..
--- charts/uninstall.sh
+++ charts/uninstall.sh
@@ -1,2 +1,11 @@
 helm uninstall auth-service-release
 helm uninstall auth-postgres
+
+helm uninstall user-service-release
+helm uninstall user-postgres
+helm uninstall user-redis
+
+helm uninstall order-service-release
+helm uninstall order-postgres
+
+helm uninstall kafka
